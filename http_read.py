b73cec169c698721dd5c3c59140ed330af3a912f
Menashe Schwob <menashe54@gmail.com>
2021-06-02 17:35:08 +0300

    add requests queue

--- http_read.py
+++ http_read.py
@@ -5,6 +5,7 @@ import aiohttp
 from asyncio import queues
 import json
 import re
+import time
 
 
 logger = logging.getLogger(__name__)
@@ -19,6 +20,9 @@ stream_handler = logging.StreamHandler()
 
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
+
+async_logger = logging.getLogger("asyncio")
+async_logger.addHandler(logging.FileHandler('async.log'))
 
 
 RECORDS_FOR_PAGE = 10
@@ -75,17 +79,34 @@ async def get_records(result=None, session=None, offset=0, **kwargs):
 async def get_total_records():
     return (await get_records())["TotalResults"]
 
-async def create_tasks(loop, queue): 
+async def request_record(requests_queue):
+    while True:
+        await requests_queue.get()
+        requests_queue.task_done()
+
+async def create_requests(records_queue, task_num):
+    requests_queue = queues.Queue()    
     async with aiohttp.ClientSession() as session:
         for i in range(0, await get_total_records(), RECORDS_FOR_PAGE):
-            # loop.create_task(get_records(offset=i, res=queue))
-            await get_records(offset=i, session=session, result=queue)
+            requests_queue.put(get_records(offset=i, session=session, result=records_queue))
+        tasks = [] 
+        for i in range(task_num):
+            tasks.append(asyncio.create_task(request_record(requests_queue, records_queue)))   
+    return requests_queue
 
 
-records = queues.Queue()
-loop = asyncio.get_event_loop()
-loop.run_until_complete(create_tasks(loop, records))
-# while(not records.empty()):
-#     print(records.get())
 
+if __name__ == "__main__":
+    async def main():       
+        task_num = 10
+        records = queues.Queue()
+        requests = create_requests(records, task_num)
+        process = []
+        
+        # while(not records.empty()):
+        #     print(records.get())
+    start = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - start
+    logging.info(f"Program completed in {elapsed:0.5f} seconds.")    
 
